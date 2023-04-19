4b5162ffbf0a5f0b20c0f738297d7d34c8943455
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-04-19 12:23:14 +0000

    [fin]投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
