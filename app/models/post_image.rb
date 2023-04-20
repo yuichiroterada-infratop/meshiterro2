6097c5ec954418545a97c9cc5d4f64722b92d080
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-04-20 06:18:03 +0000

    [fin]chapter19:いいね機能の実装

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,7 @@ class PostImage < ApplicationRecord
   
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   def get_image
     if image.attached?
@@ -11,6 +12,10 @@ class PostImage < ApplicationRecord
       file_path = Rails.root.join("app/assets/images/no_image.jpg")
       image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
     end
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
   
 end
